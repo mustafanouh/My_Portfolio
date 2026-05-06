dfba30f0314e83eb61bd18c8ebaa9a61c1da13b0
Mostafa Nouh <mustafa.nouh.nouh@gmail.com>
2026-05-06 20:17:26 +0300

    update next to 16.2.4

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
+// import createNextIntlPlugin from 'next-intl/plugin';
+
+
+// const withNextIntl = createNextIntlPlugin(
+//   './src/i18n/request.ts'
+// );
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+  
+// };
+
+// export default withNextIntl(nextConfig);
+
+
 import createNextIntlPlugin from 'next-intl/plugin';
 
+const withNextIntl = createNextIntlPlugin();
 
-const withNextIntl = createNextIntlPlugin(
-  './src/i18n/request.ts'
-);
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  
-};
+const nextConfig = {};
 
 export default withNextIntl(nextConfig);
